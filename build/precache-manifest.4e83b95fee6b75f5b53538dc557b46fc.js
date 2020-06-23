9ff36932797135f1fa5226392fe39c340fdfecee
Kalin8900 <Kalin8900@gmail.com>
2020-06-24 00:26:58 +0200

    init

---
+++ build/precache-manifest.4e83b95fee6b75f5b53538dc557b46fc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763ada889d08957c39ba949379e1f576",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "8b732827c9cdcd310a6e",
+    "url": "/app/static/css/main.fe22c35b.chunk.css"
+  },
+  {
+    "revision": "8976edbf49f7e96ae51a",
+    "url": "/app/static/js/2.9528c4bf.chunk.js"
+  },
+  {
+    "revision": "b623ef68f9185c0193acd519f5fc566d",
+    "url": "/app/static/js/2.9528c4bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b732827c9cdcd310a6e",
+    "url": "/app/static/js/main.7072bc20.chunk.js"
+  },
+  {
+    "revision": "baa489da34425f16a173",
+    "url": "/app/static/js/runtime-main.12ceb904.js"
+  },
+  {
+    "revision": "6190f72a4ff9994f910b9ff1668b1504",
+    "url": "/app/static/media/Scatter_Main.6190f72a.png"
+  },
+  {
+    "revision": "b6ec19ceacb23390aa66d2f101d0ddf6",
+    "url": "/app/static/media/github.b6ec19ce.png"
+  },
+  {
+    "revision": "653504ba454c107eced4465f4ca555e8",
+    "url": "/app/static/media/hero2.653504ba.png"
+  },
+  {
+    "revision": "7ca441a729788c9cb76f028ce23a2c43",
+    "url": "/app/static/media/lap 1.7ca441a7.svg"
+  },
+  {
+    "revision": "2cc2f10fad7cdb3ed2669531c449cc6e",
+    "url": "/app/static/media/laptopReact.2cc2f10f.svg"
+  },
+  {
+    "revision": "ee8f0bdd8e1c7b0af39928af6b5fc38a",
+    "url": "/app/static/media/tr_2.ee8f0bdd.png"
+  },
+  {
+    "revision": "886f83fdb2b6b3e2c3dcbc1648f10a0c",
+    "url": "/app/static/media/tr_3.886f83fd.png"
+  }
+]);
